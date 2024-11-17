6c5cf89f406dd4a667e9eb6d6f5b7a7904fa013b
Yeongchan Kim <hades1@kakao.com>
2024-11-17 20:44:02 +0900

    feat: Post Entity 생성

--- src/main/java/cotato/backend/domains/post/Post.java
+++ src/main/java/cotato/backend/domains/post/Post.java
@@ -1,13 +1,40 @@
 package cotato.backend.domains.post;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotBlank(message = "제목을 입력해야 합니다.")
+	private String title;
+
+	@NotBlank(message = "내용을 입력해야 합니다.")
+	private String content;
+
+	@NotBlank(message = "작성자 이름을 입력해야 합니다.")
+	private String name;
+
+	private int views;
+
+	public Post(String title, String content, String name) {
+		this.title = title;
+		this.content = content;
+		this.name = name;
+		this.views = 0;
+	}
 
 }
