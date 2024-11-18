e48e48caa37fb3861a28428d6e4141f93fc5d8d2
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-11-18 22:20:59 +0900

    feat: Post views에 인덱싱 도입

--- backend/src/main/java/cotato/backend/domains/post/entity/Post.java
+++ backend/src/main/java/cotato/backend/domains/post/entity/Post.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,9 @@ import lombok.Setter;
 
 @Entity
 @Getter
+@Table(name = "post", indexes = {
+	@Index(name = "idx_views", columnList = "views")
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
