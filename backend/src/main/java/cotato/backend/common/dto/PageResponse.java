61b679fbc22e05b660fbc8ead57a7d3ffbeec6c9
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-12-27 11:42:21 +0900

    refactor: PageResponse 응답 변경

--- backend/src/main/java/cotato/backend/common/dto/PageResponse.java
+++ backend/src/main/java/cotato/backend/common/dto/PageResponse.java
@@ -14,16 +14,16 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PageResponse<T> {
-	private List<T> items;
+	private List<T> content;
 	private int currentPage;
 	private int totalPages;
-	private long totalItems;
+	private long totalElements;
 
 	private PageResponse(List<T> content, int number, int totalPages, long totalElements) {
-		this.items = content;
+		this.content = content;
 		this.currentPage = number;
 		this.totalPages = totalPages;
-		this.totalItems = totalElements;
+		this.totalElements = totalElements;
 	}
 
 	public static <T> PageResponse<T> from(Page<T> page) {
